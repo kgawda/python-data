eded7b1c1be8ecadfcc5a0f35228ae3a1352d60c
Konrad Gawda <konrad.gawda.opensource@gmail.com>
2025-06-12 16:48:14 +0200

    Slowniki

---
+++ slowniki.py
@@ -0,0 +1,20 @@
+slownik = {"Polska": "Warszawa", "Anglia": "Londyn"}
+assert len(slownik) == 2
+slownik["Indie"] = "Bombaj"
+slownik["Indie"] = "Mumbaj"
+
+assert slownik["Polska"] == "Warszawa"
+# slownik["Francja"]  # KeyError: 'Francja'
+assert slownik.get("Francja") == None
+assert slownik.get("Francja", "wie wiadomo") == "wie wiadomo"
+assert slownik.pop("Indie")  == 'Mumbaj' #  usuwa ze słownika, zwraca 'Mumbaj'
+
+assert "Polska" in slownik  # czy klucz zawiera się w słowniku
+
+for nazwa in slownik:  # tylko klucze
+    print(nazwa)
+
+for klucz, wartosc in slownik.items():
+    print(klucz, wartosc)
+
+# poza .items() jest też: .keys() i .values()
--- warunki.py
+++ warunki.py
@@ -1,6 +1,8 @@
 liczba = 5
 napis = "Ala ma kota"
 znaki = ""
+lista = [1, 2, 3]
+tupla = (1, 2, 3)
 
 if liczba == 1:
     print("Gratuluję, liczba jest 1.")
@@ -10,3 +12,9 @@ elif znaki:
     print("Mamy jakieś znaki")
 else:
     print("Inny przypadek")
+
+assert 1 in lista
+assert 5 not in lista
+assert 1 in tupla
+assert 5 not in tupla
+assert "Ala" in napis  # uwaga! działa inaczej niż dla list/tupple, czy dany kawałek pojawia się w tekście
