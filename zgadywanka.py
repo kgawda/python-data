799df42bf8ecd967dbd00e8c2e6aee2d3b82f7b0
Konrad Gawda <konrad.gawda.opensource@gmail.com>
2025-06-13 13:10:02 +0200

    Zgadywanka dokończenie

--- zgadywanka.py
+++ zgadywanka.py
@@ -1,5 +1,25 @@
 import random
 
 sekret = random.randint(1, 100)
+zgadl = False
 
-input("Podaj liczbę: ")
+for numer_proby in range(1, 6):
+    # print("To twoja", numer_proby, "proba.")
+    strzal = input("Podaj liczbę: ")
+    try:
+        strzal = int(strzal)
+    except ValueError:
+        print("To nie jest liczba")
+        break  # kończymy zabawę
+    # else:  # jeżeli nie wystąpił żaden błąd
+    #     print("Podałeś liczbę", strzal)
+    if strzal == sekret:
+        print("BRAWO!")
+        break
+    elif strzal < sekret:
+        print("Sekretna liczba jest większa")
+    else:
+        print("Sekretna liczba jest mniejsza")
+
+if not zgadl:
+    print(f"Nie udało się. Sekretna liczba to: {sekret}")
